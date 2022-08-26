8f4b0ddbe4a6000fb97192d5b1ed7a16c908e217
Nick Yu <rednaxela64@outlook.com>
2022-08-26 16:44:38 -0700

    create counter reducer and move to index js

--- reduxapp/src/App.js
+++ reduxapp/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ReactDOM } from 'react';
+import { createStore } from 'redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const counterReducer = (state = 0, action) => {
+  switch(action.type) {
+      case 'INCREMENT':
+          return state + 1
+      case 'DECREMENT':
+          return state -1
+      case 'ZERO':
+          return 0
+      default:
+          return state
+  }
 }
+
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        {store.getState()}
+      </div>
+      <button
+        onClick={e => store.dispatch({ type: 'INCREMENT' })}
+      >
+        plus
+      </button>
+      <button
+        onClick={e => store.dispatch({ type: 'DECREMENT' })}
+      >
+        minus
+      </button>
+      <button
+        onClick={e => store.dispatch({ type: 'ZERO' })}
+      >
+        zero
+      </button>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
 
 export default App;
