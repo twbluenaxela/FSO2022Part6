deaee97859e8c0e41e4ceb7a1eceb66ea534d147
Nick Yu <rednaxela64@outlook.com>
2022-08-29 13:10:27 -0700

    add eslint

--- reduxapp/src/index.js
+++ reduxapp/src/index.js
@@ -14,33 +14,78 @@ const counterReducer = (state = 0, action) => {
           return state
   }
 }
+const noteReducer = (state = [], action) => {
+  if (action.type === 'NEW_NOTE') {
+    
+    return state.concat(action.data)
+  }
+  return state
+}
 
-const store = createStore(counterReducer)
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'TOGGLE_IMPORTANCE',
+  data: {
+    id: 2
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
 
 const App = () => {
   return (
     <div>
-      <div>
-        {store.getState()}
-      </div>
-      <button
-        onClick={e => store.dispatch({ type: 'INCREMENT' })}
-      >
-        plus
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: 'DECREMENT' })}
-      >
-        minus
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: 'ZERO' })}
-      >
-        zero
-      </button>
+      <ul>
+        {store.getState().map(note => 
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>)}
+      </ul>
     </div>
   )
 }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <div>
+//         {store.getState()}
+//       </div>
+//       <button
+//         onClick={e => store.dispatch({ type: 'INCREMENT' })}
+//       >
+//         plus
+//       </button>
+//       <button
+//         onClick={e => store.dispatch({ type: 'DECREMENT' })}
+//       >
+//         minus
+//       </button>
+//       <button
+//         onClick={e => store.dispatch({ type: 'ZERO' })}
+//       >
+//         zero
+//       </button>
+//     </div>
+//   )
+// }
 
 const renderApp = () => {
   ReactDOM.createRoot(document.getElementById('root')).render(<App />)
